3a4880c991ec95a08e1956834552f75b0404e8ab
Frank <frank@Franks-MacBook-Pro.local>
2026-02-21 23:45:48 +0800

    fix: remove progress bar from learn page

--- apps/pwa/src/app/learn/page.tsx
+++ apps/pwa/src/app/learn/page.tsx
@@ -3,7 +3,6 @@
 import { LoadingState } from "./components/LoadingState";
 import { EmptyState } from "./components/EmptyState";
 import { GuestEmptyState } from "./components/GuestEmptyState";
-import { ProgressIndicator } from "./components/ProgressIndicator";
 import { FlipCard } from "@/components/container/FlipCard";
 import { CardContent } from "./components/CardContent";
 import { RatingButtons } from "./components/RatingButtons";
@@ -42,7 +41,6 @@ function AuthenticatedLearn() {
   return (
     <div className="min-h-dvh w-full overscroll-y-none bg-linear-to-br from-blue-50 to-indigo-100 dark:from-gray-900 dark:to-gray-800 flex flex-col items-center justify-center p-4">
       <TopBar onSignOut={auth.handleSignOut} isSigningOut={auth.isSigningOut} />
-      <ProgressIndicator reviewed={progress.reviewed} total={progress.total} />
       <FlipCard
         key={currentCard.id}
         front={<CardContent side="front" knowledge={currentCard.knowledge} className="h-full w-full" onSpeak={speech.speak} />}
@@ -76,7 +74,6 @@ function GuestLearn() {
 
   return (
     <div className="min-h-dvh w-full overscroll-y-none bg-linear-to-br from-blue-50 to-indigo-100 dark:from-gray-900 dark:to-gray-800 flex flex-col items-center justify-center p-4">
-      <ProgressIndicator reviewed={progress.reviewed} total={progress.total} />
       <FlipCard
         key={currentCard.id}
         front={<CardContent side="front" knowledge={currentCard.knowledge} className="h-full w-full" onSpeak={speech.speak} />}
